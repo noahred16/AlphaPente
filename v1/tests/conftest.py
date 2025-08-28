22b9510b326b569877755a993d5d1fa5e78e718e
Noah Smith <nosmith16@gmail.com>
2025-08-27 20:34:04 -0400

    prep for cpp solutions

--- v1/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Add the parent directory to the path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- v2/src/players/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_player import BasePlayer
-from .human_player import HumanPlayer
-from .mcts_player import MCTSPlayer, EasyMCTSPlayer, MediumMCTSPlayer, HardMCTSPlayer, ExpertMCTSPlayer
-
-__all__ = ['BasePlayer', 'HumanPlayer', 'MCTSPlayer', 'EasyMCTSPlayer', 'MediumMCTSPlayer', 'HardMCTSPlayer', 'ExpertMCTSPlayer']
